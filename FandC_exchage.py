3b2e5accf8c7136e0d39a065966d024e94af98b7
shljy <shlwyyx@126.com>
2020-03-09 20:19:28 -0700

    fifth commit

---
+++ FandC_exchage.py
@@ -0,0 +1,6 @@
+#20200310
+#华氏温度到摄氏温度转换程序
+
+TF=float(input('Fahrenheit degree:')) #get Fahrenheit degree
+TC=TF*5/9-18                   #chang into Celsius temperature
+print('Fahrenheit degree:{}\nCelsius temperature:{}'.format(TF,TC)) #output two types of temperature 
